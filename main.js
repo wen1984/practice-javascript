1c646b1723021d7d9c8cbc58b1eddab340472f47
Wen Chi Yu <kyotofu28@gmail.com>
2019-10-06 18:28:44 +0800

    add this keyword and function argument

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ document.write("You can to do yourself good!!");
  bio = `${firstName} ${lastName} from a ${conutry} Learn skilled is ${skills.join(' & ')}`
 
  console.log(bio);
+
+ // 函式引數(function argument) [JavaScript學習手冊]
+ function f(today) {
+     today.message = `Learn ES6 any knowledge (previous value: '${today.message}')`;
+ }
+ let today = {
+     message: "How JS to Run?"
+ };
+ console.log(`before calling f: today.message="${today.message}"`);
+ f(today);
+ console.log(`after calling f: today.message="${today.message}"`);
+
+ // this keyword [JavaScript學習手冊]
+ const t = {
+     name: 'repeat is important',
+     speak() { return `I love to learn JavaScript ${this.name}!`; },
+ }
+ t.speak();
+ console.log(t.speak());
